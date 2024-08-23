f764ef7b4c452f0a9ccfd46d634a4815048af20d
Kwang Myung Yu <sguys99@gmail.com>
2024-08-23 20:01:48 +0900

    :sparkles: Add ch2.8
    Signed-off-by: Kwang Myung Yu <sguys99@gmail.com>

--- poet/main.py
+++ poet/main.py
@@ -1,16 +1,29 @@
-from dotenv import load_dotenv
-load_dotenv()
-import streamlit as st
-from langchain.chat_models import ChatOpenAI
-chat_model = ChatOpenAI()
-
-st.title("인공지능 시인")
-
-content = st.text_input("시의 주제을 제시해 주세요")
+# import streamlit as st
+# from langchain_community.llms.ctransformers import CTransformers
 
 
-if st.button("시 작성 요청하기"):
-    with st.spinner("시 작성중..."):
-        result = chat_model.predict(content + "에 대한 시를 써줘")
-        st.write(result)
-    
+# llm = CTransformers(
+#     model="llama-2-7b-chat.ggmlv3.q2_K.bin", #chat 모델과 llm 모델이 있음
+#     model_type="llama"
+# )
+
+# st.title("인공지능 시인")
+
+# content = st.text_input("시의 주제을 제시해 주세요")
+
+
+# if st.button("시 작성 요청하기"):
+#     with st.spinner("시 작성중..."):
+#         result = llm.predict(content + "에 대한 시를 써줘")
+#         st.write(result)
+
+# ch2.8
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(
+    model="gpt-4o",
+    temperature=0
+)
+
+result = llm.invoke("hi")
+print(result.content)
