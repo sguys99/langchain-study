27ee24623e656188dafafafe98348cb59df28940
Kwang Myung Yu <sguys99@gmail.com>
2024-08-24 21:51:19 +0900

    :recycle: Add ch3
    Signed-off-by: Kwang Myung Yu <sguys99@gmail.com>

--- poet/main.py
+++ poet/main.py
@@ -17,13 +17,34 @@
 #         result = llm.predict(content + "에 대한 시를 써줘")
 #         st.write(result)
 
-# ch2.8
+# # ch2.8
+# from langchain_openai import ChatOpenAI
+
+# llm = ChatOpenAI(
+#     model="gpt-4o",
+#     temperature=0
+# )
+
+# result = llm.invoke("hi")
+# print(result.content)
+
+#-------------------------
+# ch3
+from dotenv import load_dotenv
+import streamlit as st
 from langchain_openai import ChatOpenAI
+
+load_dotenv()
 
 llm = ChatOpenAI(
     model="gpt-4o",
     temperature=0
 )
 
-result = llm.invoke("hi")
-print(result.content)
+st.title("인공지능 시인")
+content = st.text_input("시의 주제를 제시해 주세요")
+
+if st.button("시 작성 요청하기"):
+    with st.spinner("시 작성 중"):
+        result = llm.invoke(content + "에 대한 시를 써줘")
+        st.write(result.content)
