8ebe9782635167e3d19a3461828f270e956696e0
Kwang Myung Yu <sguys99@gmail.com>
2024-08-24 22:07:18 +0900

    :recycle: Add ch4
    Signed-off-by: Kwang Myung Yu <sguys99@gmail.com>

--- poet/main.py
+++ poet/main.py
@@ -30,17 +30,34 @@
 
 #-------------------------
 # ch3
+# from dotenv import load_dotenv
+# import streamlit as st
+# from langchain_openai import ChatOpenAI
+
+# load_dotenv()
+
+# llm = ChatOpenAI(
+#     model="gpt-4o",
+#     temperature=0
+# )
+
+# st.title("인공지능 시인")
+# content = st.text_input("시의 주제를 제시해 주세요")
+
+# if st.button("시 작성 요청하기"):
+#     with st.spinner("시 작성 중"):
+#         result = llm.invoke(content + "에 대한 시를 써줘")
+#         st.write(result.content)
+#-------------------------
+# ch4: ollama serve 명령 후 진행해야 한다.
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOllama
 
 load_dotenv()
 
-llm = ChatOpenAI(
-    model="gpt-4o",
-    temperature=0
-)
-
+llm = ChatOllama(
+    model="llama3.1")
 st.title("인공지능 시인")
 content = st.text_input("시의 주제를 제시해 주세요")
 
