d03215c4bb353e21433da25e078bece03014b9d3
Kwang Myung Yu <sguys99@gmail.com>
2025-07-10 14:30:05 +0900

    :recycle: Move util files
    Signed-off-by: Kwang Myung Yu <sguys99@gmail.com>

--- fc-agent/part4/ch2.ChatGPT/utils/nodes.py
+++ fc-agent/part4/ch2.ChatGPT/utils/nodes.py
@@ -1,13 +1,21 @@
 from utils.tools import get_tools
 from langgraph.graph import MessagesState
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
 from datetime import datetime
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 llm = ChatOpenAI(
-    model="gpt-4o", 
-    api_key=os.getenv("OPENAI_API_KEY"))
+    model="gpt-4o",
+)
+
 
 def get_system_prompt(docs_info=None):
     system_prompt = f"""
@@ -22,24 +30,26 @@ def get_system_prompt(docs_info=None):
         for doc in docs_info:
             docs_context += f"- {doc['name']}: {doc['type']}\n"
         system_prompt += docs_context
-        
+
     system_prompt += "\nYou should always answer in same language as user's ask."
     return system_prompt
 
 
 def create_chatbot(docs_info=None, retriever_tool=None):
-    prompt = ChatPromptTemplate.from_messages([
-        SystemMessagePromptTemplate.from_template(get_system_prompt(docs_info)),
-        HumanMessagePromptTemplate.from_template("{input}")
-    ])
-    
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessagePromptTemplate.from_template(get_system_prompt(docs_info)),
+            HumanMessagePromptTemplate.from_template("{input}"),
+        ]
+    )
+
     # retriever_tool이 있을 경우 tools에 추가
     tools = get_tools(retriever_tool)
     llm_with_tools = llm.bind_tools(tools)
     chain = prompt | llm_with_tools
-    
+
     def chatbot(state: MessagesState):
         response = chain.invoke(state["messages"])
         return {"messages": response}
-    
+
     return chatbot
