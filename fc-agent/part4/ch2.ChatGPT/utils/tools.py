d76d6c04bc4a03638f4d6558728864b59132e649
Kwang Myung Yu <sguys99@gmail.com>
2025-07-15 10:15:44 +0900

    :recycle: Modify tools.py for langgraph chatgpt
    Signed-off-by: Kwang Myung Yu <sguys99@gmail.com>

--- fc-agent/part4/ch2.ChatGPT/utils/tools.py
+++ fc-agent/part4/ch2.ChatGPT/utils/tools.py
@@ -12,10 +12,12 @@ from langchain_core.tools import tool
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI()
+
 
 class GenImageSchema(BaseModel):
     prompt: str = Field(description="The prompt for image generation")
+
 
 @tool(args_schema=GenImageSchema)
 def generate_image(prompt: str) -> str:
@@ -23,15 +25,13 @@ def generate_image(prompt: str) -> str:
     Generate an image using DALL-E based on the given prompt.
     """
     response = client.images.generate(
-        model="dall-e-3",
-        prompt=prompt,
-        size="1024x1024",
-        quality="standard",
-        n=1
+        model="dall-e-3", prompt=prompt, size="1024x1024", quality="standard", n=1
     )
     return f"Successfuly generated the image!,{response.data[0].url}"
 
+
 repl = PythonREPL()
+
 
 @tool
 def data_visualization(code: str):
@@ -39,19 +39,22 @@ def data_visualization(code: str):
     try:
         repl.run(code)
         buf = io.BytesIO()
-        plt.savefig(buf, format='png')
+        plt.savefig(buf, format="png")
         buf.seek(0)
         img_str = base64.b64encode(buf.getvalue()).decode()
         return f"data:image/png;base64,{img_str}"
     except Exception as e:
         return f"Error creating chart: {str(e)}"
-        
+
+
 @tool
 def python_repl(code: str):
     """Execute Python code."""
     return repl.run(code)
 
+
 search = TavilySearchResults(max_results=2)
+
 
 def get_tools(retriever_tool=None):
     base_tools = [generate_image, search, python_repl, data_visualization]
