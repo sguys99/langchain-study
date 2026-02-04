d06fb372f1a39bd523f34e32633514dee0e29441
Kwang Myung Yu <sguys99@gmail.com>
2026-02-04 09:37:56 +0900

    :sparkles: Add the notebook for intro to langchain(2.1)
    Signed-off-by: Kwang Myung Yu <sguys99@gmail.com>

--- langchain-academy/intro-to-langchain/my_lecture/module2/resources/2.1_mcp_server.py
+++ langchain-academy/intro-to-langchain/my_lecture/module2/resources/2.1_mcp_server.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv("../../.env")
 
 from mcp.server.fastmcp import FastMCP
 from tavily import TavilyClient
@@ -43,17 +43,17 @@ def github_file():
 def prompt():
     """Analyze data from a langchain-ai repo file with comprehensive insights"""
     return """
-    You are a helpful assistant that answers user questions about LangChain, LangGraph and LangSmith.
+    당신은 Langchain, LangGraph, LangSmith에 대한 질문에 답변하는 친절한 어시스턴트 입니다.
 
-    You can use the following tools/resources to answer user questions:
+    질문에 답변하기 위해 다음 tools/resources를 사용할 수 있습니다.
     - search_web: Search the web for information
     - github_file: Access the langchain-ai repo files
-
-    If the user asks a question that is not related to LangChain, LangGraph or LangSmith, you should say "I'm sorry, I can only answer questions about LangChain, LangGraph and LangSmith."
-
-    You may try multiple tool and resource calls to answer the user's question.
-
-    You may also ask clarifying questions to the user to better understand their question.
+    
+    사용자가 LangChain, LangGraph or LangSmith와 관련 없는 질문을 하면, "죄송합니다. 저는 랭체인, 랭그레프, 랭스미스에 대해서만 답변할수 있어요." 라고 답변합니다.
+    
+    당신은 사용자 질문에 답변을 위해 다수의 tool과 resouce를 사용할 수 있습니다.
+    
+    당신은 사용자의 질문을 보다 잘 이해하기 위해 사용자에게 질문의 명확하게 식별하기 위한 질문을 할수 있습니다.(reasking)
     """
 
 if __name__ == "__main__":
